112a5808379e9f631a9263c0769752bbfd3b9f21
jayk1p <joukyuu@hotmail.com>
2017-05-31 13:11:51 -0700

    Add files via upload

---
+++ TipCalculator.py
@@ -0,0 +1,15 @@
+
+
+total = raw_input("Total $? ")
+
+total = int(total)
+
+print "The total is %d" %total
+
+print "For 15%,", total * 0.15
+# Notice the "comma" after the string. If this is not there,
+# the program does not work
+
+print "For 18%,", total * 0.18
+
+print "For 20%,", total * 0.20
